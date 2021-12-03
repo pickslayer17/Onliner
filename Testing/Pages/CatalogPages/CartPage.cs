b02d2065e31bf6fe7c57fe99a443671c7aadc09c
pickslayer17 <dzianis.piatrovich@gmail.com>
2021-12-03 14:43:40 +0300

    Add 5th Test

---
+++ Testing/Pages/CatalogPages/CartPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace Testing.Pages.CatalogPages
+{
+    public class CartPage : AbstractProductPage
+    {
+        public CartPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement _firstGoToOrderingButton => _driver.FindElement(By.XPath(
+            "(//div[@class='cart-form__offers-unit cart-form__offers-unit_primary'])[1]//div[contains(@class,'cart-form__offers-part_confirm')]//a"));
+
+        public void ClickFirstProductGoToOrdering() => _firstGoToOrderingButton.Click();
+    }
+}
---
+++ Testing/Pages/OrderingPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using static Testing.Extensions.WaitExtensions;
+
+namespace Testing.Pages
+{
+    public class OrderingPage : AbstractPage
+    {
+        public OrderingPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private IWebElement _orderingLogo =>
+            WaitAndGet(_driver, By.XPath("(//div[contains(@class, 'cart-form__title')])[1]"));
+
+        public override bool IsLoaded() => _orderingLogo != null;
+    }
+}
